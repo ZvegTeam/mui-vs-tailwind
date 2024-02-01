3064dc68cf0c8f3a0b8db0f32f8d1aad18c4b2dc
Dmytro Laikov <dmytro.laikov@sysgears.com>
2024-02-01 19:58:53 +0200

    Add active link styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,37 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Header: FC = () => {
+  const router = useLocation();
+
+  const path = router.pathname;
+
   return (
     <>
       <header className="main-header">
-        <h3 className='header-title'>Tailwind CSS vs Material UI</h3>
-        <nav className='nav'>
-          <ul className='nav-list'>
-            <li className='nav-list-item'>
+        <h3 className="header-title">Tailwind CSS vs Material UI</h3>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-list-item">
               <Link className="nav-list-item-link" to="/material-ui">
-                Material UI
+                <div
+                  className={'nav-list-item'.concat(
+                    path === '/material-ui' ? ' active-link' : ''
+                  )}
+                >
+                  Material UI
+                </div>
               </Link>
             </li>
-            <li className='nav-list-item'>
+            <li className="nav-list-item">
               <Link className="nav-list-item-link" to="/tailwind-css">
-                Tailwind CSS
+                <div
+                  className={'nav-list-item'.concat(
+                    path === '/tailwind-css' ? ' active-link' : ''
+                  )}
+                >
+                  Tailwind CSS
+                </div>
               </Link>
             </li>
           </ul>
